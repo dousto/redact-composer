09f6a4c102cc654179643ca16d0616c2c90dcad4
Doug Stoeckmann <dousto@gmail.com>
2023-05-07 20:21:02 -0700

    Add a musical module for basic music concepts.

---
+++ src/musical/mod.rs
@@ -0,0 +1,270 @@
+use std::fmt;
+
+#[cfg(test)]
+mod test;
+
+/// Utility struct used for operating with a set of base notes ([u8] values `0..=11`).
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Notes {
+    /// Set of [u8] note values intended -- but not enforced -- to be between `0..=11`.
+    /// These correspond to the 12 tones of a chromatic scale, with 0 representing C.
+    base_notes: Vec<u8>
+}
+
+impl Notes {
+    /// Scales the set of base notes, producing all note values
+    /// of the same pitch classes within the given range.
+    /// 
+    /// # Example
+    /// 
+    /// ```
+    /// let c_major = Key { tonic: 0, scale: Scale::Major};
+    /// let c_major_scale_notes = Notes::from(c_major.scale()).in_range(60..=72);
+    /// assert_eq!(c_major_scale_notes, [60, 62, 64, 65, 67, 69, 71, 72]);
+    /// ```
+    pub fn in_range<R>(&self, range: R) -> Vec<u8>
+     where R: IntoIterator<Item = u8> {
+        let mut clamped_base_notes: Vec<u8> = self.base_notes.iter().map(|n| n % 12).collect();
+        clamped_base_notes.sort();
+        clamped_base_notes.dedup();
+        range.into_iter().filter(|n| clamped_base_notes.contains(&(n % 12))).collect()
+    }
+}
+
+impl<T, K> From<T> for Notes
+    where T: IntoIterator<Item = K>, K: Into<u8> {
+    /// Takes any any type implementing [IntoIterator] where the iteration
+    /// items implement [Into]<[u8]>.
+    /// 
+    /// # Example
+    /// ```rust
+    /// let notes = Notes.from([1,2,3]);
+    /// ```
+    fn from(value: T) -> Self {
+        Notes {
+            base_notes: value.into_iter().map(<K>::into).collect()
+        }
+    }
+}
+
+/// Represents a key signature via a tonic ([u8] value in `0..=11`) and [Scale] (e.g. Major/Minor).
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Key {
+    /// First note of the scale. (`tonic == 0` represents C)
+    pub tonic: u8,
+    /// The interval sequence (relative to the `tonic`) defining the base notes this [Key].
+    pub scale: Scale
+}
+
+impl Key {
+    /// Returns the scale notes for this [Key], starting from the `tonic` and using relative intervals
+    /// as determined by the [Scale].
+    pub fn scale(&self) -> Vec<u8> {
+        self.scale.relative_pitches().iter().map(|p| ((self.tonic % 12) + p) % 12).collect()
+    }
+
+    /// Returns the diatonic base notes for a given [Chord] in this [Key].
+    /// 
+    /// # Example
+    /// ```rust
+    /// let c_major = Key { tonic: 0, scale: Scale::Major};
+    /// let c_major_chord_notes = c_major.chord(Chord::I);
+    /// assert_eq(c_major_chord_notes, [0, 2, 4]); // C, E, G
+    /// ```
+    pub fn chord(&self, chord: Chord) -> Vec<u8> {
+        let scale = self.scale();
+        chord.degrees().iter().map(|d| scale[*d as usize]).collect()
+    }
+}
+
+/// A type representing the diatonic harmony chord variations based on [heptatonic (7-note) scales](https://en.wikipedia.org/wiki/Heptatonic_scale).
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Chord {
+    /// ```rust
+    /// assert_eq!(Chord::I.degrees(), vec![0, 2, 4])
+    /// ```
+    I,
+    /// ```rust
+    /// assert_eq!(Chord::II.degrees(), vec![1, 3, 5])
+    /// ```
+    II,
+    /// ```rust
+    /// assert_eq!(Chord::III.degrees(), vec![2, 4, 6])
+    /// ```
+    III,
+    /// ```rust
+    /// assert_eq!(Chord::IV.degrees(), vec![3, 5, 0])
+    /// ```
+    IV,
+    /// ```rust
+    /// assert_eq!(Chord::V.degrees(), vec![4, 6, 1])
+    /// ```
+    V,
+    /// ```rust
+    /// assert_eq!(Chord::VI.degrees(), vec![5, 0, 2])
+    /// ```
+    VI,
+    /// ```rust
+    /// assert_eq!(Chord::VII.degrees(), vec![6, 1, 3])
+    /// ```
+    VII
+}
+
+impl Chord {
+    const I_STR: &str = "I";
+    const II_STR: &str = "II";
+    const III_STR: &str = "III";
+    const IV_STR: &str = "IV";
+    const V_STR: &str = "V";
+    const VI_STR: &str = "VI";
+    const VII_STR: &str = "VII";
+
+    /// Returns a [Vec]<[Chord]> of all types. 
+    pub fn values() -> Vec<Chord> {
+        vec![Self::I, Self::II, Self::III, Self::IV, Self::V, Self::VI, Self::VII]
+    }
+
+    /// Returns the diatonic degrees (scale notes) represented by this [Chord].
+    pub fn degrees(&self) -> Vec<u8> {
+        match self {
+            Chord::I   => vec![0, 2, 4],
+            Chord::II  => vec![1, 3, 5],
+            Chord::III => vec![2, 4, 6],
+            Chord::IV  => vec![3, 5, 0],
+            Chord::V   => vec![4, 6, 1],
+            Chord::VI  => vec![5, 0, 2],
+            Chord::VII => vec![6, 1, 3],
+        }
+    }
+}
+
+impl fmt::Display for Chord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", String::from(*self))
+    }
+}
+
+impl From<Chord> for String {
+    fn from(value: Chord) -> Self {
+        match value {
+            Chord::I   => Chord::I_STR,
+            Chord::II  => Chord::II_STR,
+            Chord::III => Chord::III_STR,
+            Chord::IV  => Chord::IV_STR,
+            Chord::V   => Chord::V_STR,
+            Chord::VI  => Chord::VI_STR,
+            Chord::VII => Chord::VII_STR,
+        }.into()
+    }
+}
+
+impl From<&Chord> for String {
+    fn from(value: &Chord) -> Self { Self::from(*value) }
+}
+
+impl From<&str> for Chord {
+    fn from(value: &str) -> Self {
+        match value {
+            Self::I_STR   => Self::I,
+            Self::II_STR  => Self::II,
+            Self::III_STR => Self::III,
+            Self::IV_STR  => Self::IV,
+            Self::V_STR   => Self::V,
+            Self::VI_STR  => Self::VI,
+            Self::VII_STR => Self::VII,
+            _ => panic!()
+        }
+    }
+}
+
+impl From<String> for Chord {
+    fn from(value: String) -> Self { Self::from(value.as_str()) }
+}
+
+impl From<&String> for Chord {
+    fn from(value: &String) -> Self { Self::from(value.as_str()) }
+}
+
+/// A type representing the sequence of intervals defining the notes in a [Key].
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Scale {
+    /// ```rust
+    /// assert_eq!(Scale::Major.relative_pitches(), vec![0, 2, 4, 5, 7, 9, 11])
+    /// ```
+    Major,
+    /// ```rust
+    /// assert_eq!(Scale::Minor.relative_pitches(), vec![0, 2, 3, 5, 7, 9, 10])
+    /// ```
+    Minor,
+    /// ```rust
+    /// assert_eq!(Scale::NaturalMinor.relative_pitches(), vec![0, 2, 3, 5, 7, 8, 10])
+    /// ```
+    NaturalMinor,
+    /// ```rust
+    /// assert_eq!(Scale::HarmonicMinor.relative_pitches(), vec![0, 2, 3, 5, 7, 8, 11])
+    /// ```
+    HarmonicMinor
+}
+
+impl Scale {
+    const MAJOR_STR: &str = "Major";
+    const MINOR_STR: &str = "Minor";
+    const NATURAL_MINOR_STR: &str = "NaturalMinor";
+    const HARMONIC_MINOR_STR: &str = "HarmonicMinor";
+
+    /// Returns a [Vec]<[Scale]> of all types. 
+    pub fn values() -> Vec<Scale> {
+        vec![Self::Major, Self::Minor, Self::NaturalMinor, Self::HarmonicMinor]
+    }
+
+    // Returns the pitches of this [Scale] (via interval offsest from tonic).
+    pub fn relative_pitches(&self) -> Vec<u8> {
+        match self {
+                    Scale::Major => vec![0, 2, 4, 5, 7, 9, 11],
+                    Scale::Minor => vec![0, 2, 3, 5, 7, 9, 10],
+             Scale::NaturalMinor => vec![0, 2, 3, 5, 7, 8, 10],
+            Scale::HarmonicMinor => vec![0, 2, 3, 5, 7, 8, 11],
+        }
+    }
+}
+
+impl fmt::Display for Scale {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", String::from(self))
+    }
+}
+
+impl From<Scale> for String {
+    fn from(value: Scale) -> Self {
+        match value {
+            Scale::Major         => Scale::MAJOR_STR,
+            Scale::Minor         => Scale::MINOR_STR,
+            Scale::NaturalMinor  => Scale::NATURAL_MINOR_STR,
+            Scale::HarmonicMinor => Scale::HARMONIC_MINOR_STR,
+        }.into()
+    }
+}
+
+impl From<&Scale> for String {
+    fn from(value: &Scale) -> Self { Self::from(*value) }
+}
+
+impl From<&str> for Scale {
+    fn from(value: &str) -> Self {
+        match value {
+            Self::MAJOR_STR          => Self::Major,
+            Self::MINOR_STR          => Self::Minor,
+            Self::NATURAL_MINOR_STR  => Self::NaturalMinor,
+            Self::HARMONIC_MINOR_STR => Self::HarmonicMinor,
+            _ => panic!()
+        }
+    }
+}
+
+impl From<String> for Scale {
+    fn from(value: String) -> Self { Self::from(value.as_str()) }
+}
+
+impl From<&String> for Scale {
+    fn from(value: &String) -> Self { Self::from(value.as_str()) }
+}
---
+++ src/musical/test.rs
@@ -0,0 +1,145 @@
+use crate::musical::{Scale, Chord, Key};
+
+use super::Notes;
+
+#[test]
+fn note_ranges() {
+    assert_eq!(
+        Notes::from(vec![0,1,2,9,10,11]).in_range(24..=38),
+        [24, 25, 26, 33, 34, 35, 36, 37, 38]
+    )
+}
+
+#[test]
+fn note_range_works_with_notes_outside_1_to_12_range() {
+    let notes = Notes::from(vec![0,1,2,9,10,11]);
+    assert_eq!(Notes::from(notes.in_range(24..=38)).in_range(24..=38), [24, 25, 26, 33, 34, 35, 36, 37, 38]);
+}
+
+#[test]
+fn middle_c_major_scale() {
+    assert_eq!(Notes::from(Key { tonic: 0, scale: Scale::Major }.scale()).in_range(60..=72), [60, 62, 64, 65, 67, 69, 71, 72])
+}
+
+#[test]
+fn middle_c_minor_scale() {
+    assert_eq!(Notes::from(Key { tonic: 0, scale: Scale::Minor }.scale()).in_range(60..=72), [60, 62, 63, 65, 67, 69, 70, 72])
+}
+
+#[test]
+fn middle_c_natural_minor_scale() {
+    assert_eq!(Notes::from(Key { tonic: 0, scale: Scale::NaturalMinor }.scale()).in_range(60..=72), [60, 62, 63, 65, 67, 68, 70, 72])
+}
+
+#[test]
+fn middle_c_harmonic_minor_scale() {
+    assert_eq!(Notes::from(Key { tonic: 0, scale: Scale::HarmonicMinor }.scale()).in_range(60..=72), [60, 62, 63, 65, 67, 68, 71, 72])
+}
+
+#[test]
+fn chord_i_degrees() {
+    assert_eq!(Chord::I.degrees(), [0, 2, 4])
+}
+
+#[test]
+fn chord_ii_degrees() {
+    assert_eq!(Chord::II.degrees(), [1, 3, 5])
+}
+
+#[test]
+fn chord_iii_degrees() {
+    assert_eq!(Chord::III.degrees(), [2, 4, 6])
+}
+
+#[test]
+fn chord_iv_degrees() {
+    assert_eq!(Chord::IV.degrees(), [3, 5, 0])
+}
+
+#[test]
+fn chord_v_degrees() {
+    assert_eq!(Chord::V.degrees(), [4, 6, 1])
+}
+
+#[test]
+fn chord_vi_degrees() {
+    assert_eq!(Chord::VI.degrees(), [5, 0, 2])
+}
+
+#[test]
+fn chord_vii_degrees() {
+    assert_eq!(Chord::VII.degrees(), [6, 1, 3])
+}
+
+// region: Chord to/from String conversion tests
+
+#[test]
+fn chord_i_to_string() { assert_eq!(Chord::I.to_string(), Chord::I_STR) }
+
+#[test]
+fn chord_i_from_string() { assert_eq!(Chord::from(Chord::I_STR), Chord::I) }
+
+#[test]
+fn chord_ii_to_string() { assert_eq!(Chord::II.to_string(), Chord::II_STR) }
+
+#[test]
+fn chord_ii_from_string() { assert_eq!(Chord::from(Chord::II_STR), Chord::II) }
+
+#[test]
+fn chord_iii_to_string() { assert_eq!(Chord::III.to_string(), Chord::III_STR) }
+
+#[test]
+fn chord_iii_from_string() { assert_eq!(Chord::from(Chord::III_STR), Chord::III) }
+
+#[test]
+fn chord_iv_to_string() { assert_eq!(Chord::IV.to_string(), Chord::IV_STR) }
+
+#[test]
+fn chord_iv_from_string() { assert_eq!(Chord::from(Chord::IV_STR), Chord::IV) }
+
+#[test]
+fn chord_v_to_string() { assert_eq!(Chord::V.to_string(), Chord::V_STR) }
+
+#[test]
+fn chord_v_from_string() { assert_eq!(Chord::from(Chord::V_STR), Chord::V) }
+
+#[test]
+fn chord_vi_to_string() { assert_eq!(Chord::VI.to_string(), Chord::VI_STR) }
+
+#[test]
+fn chord_vi_from_string() { assert_eq!(Chord::from(Chord::VI_STR), Chord::VI) }
+
+#[test]
+fn chord_vii_to_string() { assert_eq!(Chord::VII.to_string(), Chord::VII_STR) }
+
+#[test]
+fn chord_vii_from_string() { assert_eq!(Chord::from(Chord::VII_STR), Chord::VII) }
+
+// endregion: Chord to/from String conversion tests
+
+// region: Scale to/from String conversion tests
+#[test]
+fn major_to_string() { assert_eq!(Scale::Major.to_string(), Scale::MAJOR_STR) }
+
+#[test]
+fn major_from_string() { assert_eq!(Scale::from(Scale::MAJOR_STR), Scale::Major) }
+
+#[test]
+fn minor_to_string() { assert_eq!(Scale::Minor.to_string(), Scale::MINOR_STR) }
+
+#[test]
+fn minor_from_string() { assert_eq!(Scale::from(Scale::MINOR_STR), Scale::Minor) }
+
+#[test]
+fn natural_minor_to_string() { assert_eq!(Scale::NaturalMinor.to_string(), Scale::NATURAL_MINOR_STR) }
+
+#[test]
+fn natural_minor_from_string() { assert_eq!(Scale::from(Scale::NATURAL_MINOR_STR), Scale::NaturalMinor) }
+
+#[test]
+fn harmonic_minor_to_string() { assert_eq!(Scale::HarmonicMinor.to_string(), Scale::HARMONIC_MINOR_STR) }
+
+#[test]
+fn harmonic_minor_from_string() { assert_eq!(Scale::from(Scale::HARMONIC_MINOR_STR), Scale::HarmonicMinor) }
+
+// endregion String conversion tests
